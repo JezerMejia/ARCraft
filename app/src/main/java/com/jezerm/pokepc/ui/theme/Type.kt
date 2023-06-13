656f60b402e5c22a6cd5b928e01a3d08b48fd232
Jezer Mejía <amyuki4@icloud.com>
2023-06-13 14:02:35 -0600

    Added Unifont and type definitions

--- app/src/main/java/com/jezerm/pokepc/ui/theme/Type.kt
+++ app/src/main/java/com/jezerm/pokepc/ui/theme/Type.kt
@@ -2,27 +2,40 @@ package com.jezerm.pokepc.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.jezerm.pokepc.R
+
+val unifont = FontFamily(
+    Font(R.font.unifont15),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    defaultFontFamily = unifont,
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
         fontWeight = FontWeight.W500,
+        fontSize = 16.sp
+    ),
+    button = TextStyle(
+        fontWeight = FontWeight.Black,
         fontSize = 14.sp
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+    h1 = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 52.sp,
+    ),
+    h2 = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp,
     )
-    */
+/* Other default text styles to override
+caption = TextStyle(
+    fontFamily = FontFamily.Default,
+    fontWeight = FontWeight.Normal,
+    fontSize = 12.sp
+)
+*/
 )
