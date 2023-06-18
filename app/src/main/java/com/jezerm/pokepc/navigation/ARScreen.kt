b664759ff510faffc3bb46c6983a6423d1bc1db4
mmajewsky133 <martin.majewsky13368@est.uca.edu.ni>
2023-06-18 16:29:25 -0600

    Added ARScreen UI Prototype

--- app/src/main/java/com/jezerm/pokepc/navigation/ARScreen.kt
+++ app/src/main/java/com/jezerm/pokepc/navigation/ARScreen.kt
@@ -1,14 +1,23 @@
 package com.jezerm.pokepc.navigation
 
 import android.graphics.BitmapFactory
-import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 import io.github.sceneview.ar.ARScene
 import io.github.sceneview.ar.node.ArModelNode
 import io.github.sceneview.ar.node.ArNode
@@ -38,6 +47,26 @@ fun ARScreen() {
     }
     nodes.add(earthNode)
 
+    val constraints = ConstraintSet {
+        val inventoryBox = createRefFor("inventoryBox")
+        val timerBox = createRefFor("timerBox")
+
+        constrain(inventoryBox) {
+            bottom.linkTo(parent.bottom)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+            width = Dimension.matchParent
+            height = Dimension.value(80.dp)
+        }
+        constrain(timerBox) {
+            top.linkTo(parent.top)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+            width = Dimension.value(200.dp)
+            height = Dimension.value(80.dp)
+        }
+    }
+
     Box(modifier = Modifier.fillMaxSize()) {
         ARScene(
             modifier = Modifier.fillMaxSize(),
@@ -56,5 +85,41 @@ fun ARScreen() {
                 // User tapped in the AR view
             }
         )
+        ConstraintLayout(constraints, modifier = Modifier.fillMaxSize()) {
+            Box(
+                modifier = Modifier
+                    .background(Color.Gray)
+                    .layoutId("inventoryBox")
+            ) {
+                Row {
+                    Box(
+                        modifier = Modifier
+                            .size(80.dp)
+                            .background(color = Color.Black)
+                            .border(5.dp, Color.Gray)
+                    )
+                    Box(
+                        modifier = Modifier
+                            .size(80.dp)
+                            .background(color = Color.Black)
+                            .border(5.dp, Color.Gray)
+                    )
+                    Box(
+                        modifier = Modifier
+                            .size(80.dp)
+                            .background(color = Color.Black)
+                            .border(5.dp, Color.Gray)
+                    )
+                }
+            }
+
+            Box(
+                modifier = Modifier
+                    .background(Color.Gray)
+                    .layoutId("timerBox")
+                    .background(color = Color.Black)
+                    .border(5.dp, Color.Gray)
+            )
+        }
     }
 }
