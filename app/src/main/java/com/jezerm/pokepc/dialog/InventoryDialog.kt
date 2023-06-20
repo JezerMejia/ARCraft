103787c9747877503a4640c27f94d0defb8f9e41
mmajewsky133 <martin.majewsky13368@est.uca.edu.ni>
2023-06-20 15:30:56 -0600

    Updated Inventory Dialog UI

--- app/src/main/java/com/jezerm/pokepc/dialog/InventoryDialog.kt
+++ app/src/main/java/com/jezerm/pokepc/dialog/InventoryDialog.kt
@@ -1,5 +1,6 @@
 package com.jezerm.pokepc.dialog
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -10,10 +11,14 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
@@ -21,25 +26,73 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.FilterQuality
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import com.jezerm.pokepc.entities.Inventory
+import com.jezerm.pokepc.entities.Item
 import com.jezerm.pokepc.ui.components.TextShadow
+import com.jezerm.pokepc.ui.modifiers.insetBorder
+import com.jezerm.pokepc.ui.modifiers.outsetBorder
 import com.jezerm.pokepc.ui.theme.PixelBorderShape
+
+@Composable
+fun InventoryGrid() {
+    val items = ArrayList<Pair<Item, Int>>()
+
+    for (i in 1..20) {
+        items.add(Item.AIR to i)
+    }
+
+    LazyVerticalGrid(
+        modifier = Modifier
+            .widthIn(100.dp, 200.dp)
+            .heightIn(120.dp, 400.dp),
+        columns = GridCells.Fixed(4),
+        userScrollEnabled = false
+    ) {
+        items(items, key = { c -> c.second }) { (item, position) ->
+            val imageBitmap = ImageBitmap.imageResource(item.image)
+            Surface(color = Color(139, 139, 139)) {
+                Image(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight()
+                        .clip(RectangleShape)
+                        .insetBorder(lightSize = 4.dp, darkSize = 4.dp, borderPadding = 0.dp)
+                        .padding(4.dp),
+                    bitmap = imageBitmap,
+                    filterQuality = FilterQuality.None,
+                    contentDescription = item.value,
+                    contentScale = ContentScale.FillWidth,
+                    alignment = Alignment.Center
+                )
+            }
+        }
+    }
+}
 
 @Composable
 fun InventoryDialog(setShowDialog: (Boolean) -> Unit) {
 
-    val data = remember { mutableStateOf(List(20) { "Item $it" }) }
+    val grayColor = Color(198, 198, 198)
 
     Dialog(onDismissRequest = { setShowDialog(false) }) {
-        Surface(
-            shape = PixelBorderShape(),
-            color = Color(143, 143, 143)
-        ) {
-            Box(
-                contentAlignment = Alignment.Center
+        Surface {
+            Card(
+                modifier = Modifier
+                    .clip(RectangleShape)
+                    .outsetBorder(lightSize = 8.dp, darkSize = 10.dp, borderPadding = 0.dp),
+                shape = RectangleShape,
+                backgroundColor = grayColor
             ) {
                 Column(modifier = Modifier.padding(24.dp)) {
                     Row(
@@ -54,26 +107,14 @@ fun InventoryDialog(setShowDialog: (Boolean) -> Unit) {
                         )
                     }
 
-                    Spacer(modifier = Modifier.height(24.dp))
+                    Spacer(modifier = Modifier.height(16.dp))
 
                     Row(
                         modifier = Modifier
                             .fillMaxWidth(),
                         horizontalArrangement = Arrangement.Center
-                    ){
-                        LazyVerticalGrid(
-                            columns = GridCells.Fixed(4),
-                            modifier = Modifier
-                        ) {
-                            items(data.value) { item ->
-                                Box (
-                                    modifier = Modifier
-                                        .aspectRatio(1f)
-                                        .background(Color(143, 143, 143))
-                                        .border(2.dp, Color(85, 85, 85))
-                                )
-                            }
-                        }
+                    ) {
+                        InventoryGrid()
                     }
                 }
             }
