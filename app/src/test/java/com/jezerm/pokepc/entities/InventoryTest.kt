251c79e03e6dcdce6a40e4f640d18fb4ca893fc6
Jezer Mejía <amyuki4@icloud.com>
2023-06-19 19:48:46 -0600

    Styling

--- app/src/test/java/com/jezerm/pokepc/entities/InventoryTest.kt
+++ app/src/test/java/com/jezerm/pokepc/entities/InventoryTest.kt
@@ -1,7 +1,8 @@
 package com.jezerm.pokepc.entities
 
 import com.jezerm.pokepc.data.ItemDto
-import org.junit.Assert.*
+import org.junit.Assert.assertArrayEquals
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class InventoryTest {
@@ -13,24 +14,31 @@ class InventoryTest {
         inventory.addItem(Item.WOOD, 4)
         inventory.addItem(Item.OBSIDIAN, 2)
 
-        assertArrayEquals(arrayOf(
-            ItemDto(Item.GLASS, 1, 1),
-            ItemDto(Item.WOOD, 4, 2),
-            ItemDto(Item.OBSIDIAN, 2, 3),
-        ), inventory.items.toArray())
+        assertArrayEquals(
+            arrayOf(
+                ItemDto(Item.GLASS, 1, 1),
+                ItemDto(Item.WOOD, 4, 2),
+                ItemDto(Item.OBSIDIAN, 2, 3),
+            ), inventory.items.toArray()
+        )
     }
+
     @Test
     fun addSameItem() {
         inventory.addItem(Item.GLASS, 2)
 
-        assertArrayEquals(arrayOf(
-            ItemDto(Item.GLASS, 2, 1),
-        ), inventory.items.toArray())
+        assertArrayEquals(
+            arrayOf(
+                ItemDto(Item.GLASS, 2, 1),
+            ), inventory.items.toArray()
+        )
 
         inventory.addItem(Item.GLASS, 4)
-        assertArrayEquals(arrayOf(
-            ItemDto(Item.GLASS, 6, 1),
-        ), inventory.items.toArray())
+        assertArrayEquals(
+            arrayOf(
+                ItemDto(Item.GLASS, 6, 1),
+            ), inventory.items.toArray()
+        )
     }
 
     @Test
@@ -42,10 +50,12 @@ class InventoryTest {
         inventory.removeItem(Item.WOOD, 4)
         inventory.removeItem(Item.OBSIDIAN)
 
-        assertArrayEquals(arrayOf(
-            ItemDto(Item.GLASS, 1, 1),
-            ItemDto(Item.OBSIDIAN, 1, 3),
-        ), inventory.items.toArray())
+        assertArrayEquals(
+            arrayOf(
+                ItemDto(Item.GLASS, 1, 1),
+                ItemDto(Item.OBSIDIAN, 1, 3),
+            ), inventory.items.toArray()
+        )
     }
 
     @Test
