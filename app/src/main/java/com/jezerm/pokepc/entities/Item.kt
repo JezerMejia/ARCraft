4c7c20ca870a69fa6334b1ffde014a292c77da03
Jezer Mejía <amyuki4@icloud.com>
2023-06-20 11:46:40 -0600

    Added Item images

--- app/src/main/java/com/jezerm/pokepc/entities/Item.kt
+++ app/src/main/java/com/jezerm/pokepc/entities/Item.kt
@@ -1,31 +1,34 @@
 package com.jezerm.pokepc.entities
 
-enum class Item(val value: String, val stackable: Boolean = true) {
+import com.jezerm.pokepc.R
+
+enum class Item(val value: String, val image: Int = 0, val stackable: Boolean = true) {
     // Basic items
-    SAND("Arena"),
-    WOOD("Madera"),
-    STICK("Palo"),
-    IRON("Hierro"),
-    DIAMOND("Diamante"),
-    SUGAR_CANE("Caña de azúcar"),
+    AIR("", R.drawable.air, false),
+    SAND("Arena", R.drawable.sand),
+    WOOD("Madera", R.drawable.oak_planks),
+    STICK("Palo", R.drawable.stick),
+    IRON("Hierro", R.drawable.iron_ingot),
+    DIAMOND("Diamante", R.drawable.diamond),
+    SUGAR_CANE("Caña de azúcar", R.drawable.sugar_cane),
 
     // Cake ingredients
-    MILK_BUCKET("Cubo con leche"),
-    SUGAR("Azúcar"),
-    EGG("Huevo"),
-    WHEAT("Trigo"),
+    MILK_BUCKET("Cubo con leche", R.drawable.milk_bucket),
+    SUGAR("Azúcar", R.drawable.sugar),
+    EGG("Huevo", R.drawable.egg),
+    WHEAT("Trigo", R.drawable.wheat),
 
     // Beacon ingredients
-    GLASS("Vidrio"),
-    NETHER_STAR("Estrella del Nether", false),
-    OBSIDIAN("Obsidiana"),
+    GLASS("Vidrio", R.drawable.glass),
+    NETHER_STAR("Estrella del Nether", R.drawable.nether_star, false),
+    OBSIDIAN("Obsidiana", R.drawable.obsidian),
 
     // Tools or actionable items
-    BUCKET("Cubo"),
-    IRON_PICKAXE("Pico de hierro", false),
-    DIAMOND_SWORD("Espada de diamante", false),
+    BUCKET("Cubo", R.drawable.bucket),
+    IRON_PICKAXE("Pico de hierro", R.drawable.iron_pickaxe, false),
+    DIAMOND_SWORD("Espada de diamante", R.drawable.diamond_sword, false),
 
     // End items
-    CAKE("Pastel"),
-    BEACON("Beacon"),
+    CAKE("Pastel", R.drawable.cake),
+    BEACON("Beacon", R.drawable.beacon),
 }
