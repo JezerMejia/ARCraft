62d3d392f7c1484c0b0ac1830087563829d7d782
Jezer Mejía <amyuki4@icloud.com>
2023-06-19 19:24:50 -0600

    Added Inventory Android tests

---
+++ app/src/androidTest/java/com/jezerm/pokepc/entities/InventoryTest.kt
@@ -0,0 +1,71 @@
+package com.jezerm.pokepc.entities
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.jezerm.pokepc.data.ItemDto
+import com.jezerm.pokepc.data.ItemDtoRoomDatabase
+import com.jezerm.pokepc.data.RoomRepository
+import kotlinx.coroutines.*
+import kotlinx.coroutines.test.*
+import org.junit.After
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+
+@RunWith(AndroidJUnit4::class)
+class InventoryTest {
+    private val inventory = Inventory()
+    private lateinit var db: ItemDtoRoomDatabase
+
+    @Before
+    fun createDb() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(
+            context, ItemDtoRoomDatabase::class.java
+        ).build()
+
+        RoomRepository.init(db.itemDtoDao())
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        RoomRepository.close()
+        db.close()
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class, DelicateCoroutinesApi::class)
+    @Test
+    fun initFromDatabase() = runTest(UnconfinedTestDispatcher()) {
+        GlobalScope.launch(Dispatchers.IO) {
+            inventory.initFromDatabase()
+            assertEquals(0, inventory.items.size)
+
+            val repository = RoomRepository.getInstance()
+            repository.insert(ItemDto(Item.WOOD, 1, 1))
+
+            inventory.initFromDatabase()
+            assertEquals(1, inventory.items.size)
+        }.join()
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class, DelicateCoroutinesApi::class)
+    @Test
+    fun saveToDatabase() = runTest(UnconfinedTestDispatcher()) {
+        GlobalScope.launch(Dispatchers.IO) {
+            inventory.addItem(Item.GLASS)
+            inventory.addItem(Item.WOOD, 4)
+            assertEquals(2, inventory.items.size)
+
+            inventory.saveToDatabase()
+            inventory.initFromDatabase()
+            assertEquals(2, inventory.items.size)
+            assert(inventory.items.any { v -> v.item == Item.GLASS })
+            assert(inventory.items.any { v -> v.item == Item.WOOD && v.quantity == 4 })
+        }.join()
+    }
+}
