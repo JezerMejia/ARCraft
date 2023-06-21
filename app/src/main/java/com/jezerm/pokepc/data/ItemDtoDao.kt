50dce75e14c8ed15d4c9b861731d081ab334be50
Jezer Mejía <amyuki4@icloud.com>
2023-06-20 21:33:31 -0600

    Added inventoryId to ItemDto

--- app/src/main/java/com/jezerm/pokepc/data/ItemDtoDao.kt
+++ app/src/main/java/com/jezerm/pokepc/data/ItemDtoDao.kt
@@ -1,6 +1,7 @@
 package com.jezerm.pokepc.data
 
 import androidx.room.*
+import com.jezerm.pokepc.entities.Inventory
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -8,7 +9,14 @@ interface ItemDtoDao {
     @Query("SELECT * FROM tblItem")
     fun getAll(): Flow<List<ItemDto>>
 
-    @Insert
+    @Query("SELECT * FROM tblItem WHERE inventoryId = :inventoryId")
+    fun getInventory(inventoryId: Int): Flow<List<ItemDto>>
+
+    fun getInventory(inventory: Inventory): Flow<List<ItemDto>> {
+        return getInventory(inventory.getId())
+    }
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertItemDto(itemDto: ItemDto)
 
     @Update
