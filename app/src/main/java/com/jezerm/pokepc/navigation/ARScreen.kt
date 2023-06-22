9d37931f999e0a128d65d73fbb3057d00365667d
Jezer Mejía <amyuki4@icloud.com>
2023-06-22 03:45:01 -0600

    Added a new launcher icon Added a custom Splash screen Improved the UI

--- app/src/main/java/com/jezerm/pokepc/navigation/ARScreen.kt
+++ app/src/main/java/com/jezerm/pokepc/navigation/ARScreen.kt
@@ -5,17 +5,7 @@ import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -176,7 +166,7 @@ fun ARScreen() {
                 }
             },
         )
-        ConstraintLayout(constraints, modifier = Modifier.fillMaxSize()) {
+        ConstraintLayout(constraints, modifier = Modifier.fillMaxSize().displayCutoutPadding()) {
             Surface(modifier = Modifier.layoutId("inventoryBox")) {
                 Card(
                     modifier = Modifier
