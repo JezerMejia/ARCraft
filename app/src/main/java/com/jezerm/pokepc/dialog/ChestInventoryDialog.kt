783713ca6de09dbca26a5a3de7cff5d39ada9b23
mmajewsky133 <martin.majewsky13368@est.uca.edu.ni>
2023-06-22 05:01:24 -0600

    Added Chest Preview to Dialog

--- app/src/main/java/com/jezerm/pokepc/dialog/ChestInventoryDialog.kt
+++ app/src/main/java/com/jezerm/pokepc/dialog/ChestInventoryDialog.kt
@@ -6,10 +6,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -19,6 +21,8 @@ import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -28,9 +32,12 @@ import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import com.jezerm.pokepc.R
+import com.jezerm.pokepc.entities.Chest
 import com.jezerm.pokepc.entities.Inventory
 import com.jezerm.pokepc.entities.Item
 import com.jezerm.pokepc.ui.components.TextShadow
@@ -38,7 +45,7 @@ import com.jezerm.pokepc.ui.modifiers.insetBorder
 import com.jezerm.pokepc.ui.modifiers.outsetBorder
 
 @Composable
-fun ChestGrid(inventory: Inventory) {
+fun ChestGrid() {
     val items = ArrayList<Pair<Item, Int>>()
 
     for (i in 1..8) {
@@ -74,7 +81,7 @@ fun ChestGrid(inventory: Inventory) {
 }
 
 @Composable
-fun InventoryGrid(inventory: Inventory) {
+private fun InventoryGrid() {
     val items = ArrayList<Pair<Item, Int>>()
 
     for (i in 1..20) {
@@ -110,7 +117,8 @@ fun InventoryGrid(inventory: Inventory) {
 }
 
 @Composable
-fun ChestInventoryDialog(setShowDialog: (Boolean) -> Unit, chestName: String, inventory: Inventory) {
+fun ChestInventoryDialog(setShowDialog: (Boolean) -> Unit, chestType: Int) {
+
     val grayColor = Color(198, 198, 198)
 
     Dialog(onDismissRequest = { setShowDialog(false) }) {
@@ -128,8 +136,31 @@ fun ChestInventoryDialog(setShowDialog: (Boolean) -> Unit, chestName: String, in
                             .fillMaxWidth(),
                         horizontalArrangement = Arrangement.Center
                     ) {
+                        val chestImg = if (chestType == Chest.ChestType.ONE.value) {
+                            R.drawable.chest
+                        } else if (chestType == Chest.ChestType.TWO.value) {
+                            R.drawable.ender_chest
+                        } else {
+                            R.drawable.xmas_chest
+                        }
+
+                        Image(
+                            painter = painterResource(id = chestImg),
+                            contentDescription = "",
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier.size(200.dp)
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
                         TextShadow(
-                            text = "Cofre $chestName",
+                            text = "Cofre",
                             style = MaterialTheme.typography.h3,
                             textAlign = TextAlign.Center
                         )
@@ -142,7 +173,7 @@ fun ChestInventoryDialog(setShowDialog: (Boolean) -> Unit, chestName: String, in
                             .fillMaxWidth(),
                         horizontalArrangement = Arrangement.Center
                     ) {
-                        ChestGrid(inventory)
+                        ChestGrid()
                     }
 
                     Spacer(modifier = Modifier.height(24.dp))
@@ -166,7 +197,7 @@ fun ChestInventoryDialog(setShowDialog: (Boolean) -> Unit, chestName: String, in
                             .fillMaxWidth(),
                         horizontalArrangement = Arrangement.Center
                     ) {
-                        InventoryGrid(inventory)
+                        InventoryGrid()
                     }
                 }
             }
