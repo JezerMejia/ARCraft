96703f09836d26aa65a67ed017b7fdfa27bc76df
Jezer Mejía <amyuki4@icloud.com>
2023-06-24 01:34:33 -0600

    Added Raw Iron item and Iron BurnRecipe

--- app/src/main/java/com/jezerm/pokepc/entities/BurnRecipe.kt
+++ app/src/main/java/com/jezerm/pokepc/entities/BurnRecipe.kt
@@ -1,5 +1,6 @@
 package com.jezerm.pokepc.entities
 
 enum class BurnRecipe(val item: Item, val result: Item) {
-    GLASS(Item.SAND, Item.GLASS)
+    GLASS(Item.SAND, Item.GLASS),
+    IRON(Item.RAW_IRON, Item.IRON)
 }
